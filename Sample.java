2cdad1d379663b0874c0cfe72692eb3f923e3392
Sunil <csunilk2002@gmail.com>
2025-08-26 12:28:38 +0530

    test created

--- Sample.java
+++ Sample.java
@@ -3,5 +3,15 @@ public class Sample{
 		System.out.println("Hello World");
 		System.out.println("Made changes");
 		System.out.println("Evening update");
+
+Hello hello = new Hello();
+        String result = hello.greet();
+
+        if ("Hello from Jenkins!".equals(result)) {
+            System.out.println("TEST PASSED ✅");
+        } else {
+            System.out.println("TEST FAILED ❌, got: " + result);
+            System.exit(1); // mark pipeline as failed
+        }
 	}
 }
